f3e427375e855474c44d8d72990eaaaf6d1773f4
Ezio123-Kenway <eziokenway054@gmail.com>
2023-11-03 14:43:11 +0630

    handled tables page

--- src/pages/backoffice/tables/index.tsx
+++ src/pages/backoffice/tables/index.tsx
@@ -1,18 +1,27 @@
 import { ItemCard } from "@/components/ItemCard";
 import { NewTable } from "@/components/NewTable";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { setTablesByLocationId } from "@/store/slices/tableSlice";
 import TableBarIcon from "@mui/icons-material/TableBar";
 import { Box, Button } from "@mui/material";
 import { Table } from "@prisma/client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const TablesPage = () => {
   const [open, setOpen] = useState(false);
   const tables = useAppSelector((state) => state.table.items);
   const [selectedTables, setSelectedTables] = useState<Table[]>([]);
-  const dispatch = useAppDispatch();
 
-  if (!selectedTables.length) return null;
+  useEffect(() => {
+    if (tables) {
+      const locationId = Number(localStorage.getItem("selectedLocationId"));
+      const filteredTables = tables.filter(
+        (table) => table.locationId === locationId
+      );
+      setSelectedTables(filteredTables);
+    }
+  }, [tables]);
+
   return (
     <Box>
       <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
@@ -21,14 +30,15 @@ const TablesPage = () => {
         </Button>
       </Box>
       <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-        {tables.map((item) => (
-          <ItemCard
-            href={`/backoffice/tables/${item.id}`}
-            icon={<TableBarIcon />}
-            key={item.id}
-            title={item.name}
-          />
-        ))}
+        {selectedTables.length > 0 &&
+          selectedTables.map((item) => (
+            <ItemCard
+              href={`/backoffice/tables/${item.id}`}
+              icon={<TableBarIcon />}
+              key={item.id}
+              title={item.name}
+            />
+          ))}
       </Box>
       <NewTable open={open} setOpen={setOpen} />
     </Box>
